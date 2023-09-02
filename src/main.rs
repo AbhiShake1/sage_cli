84675e112ce0fe0d45631621ff8d14a7e3329a25
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-09-02 18:14:44 +0545

    setup clap with feature flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Sage CLI")
+        .version("1.0")
+        .author("AbhiShake1")
+        .about("CLI to interact with the sage project")
+        .arg(arg!(-f --feature <featurename>).required(true))
+        .get_matches();
+
+    println!(
+        "feature: {:?}",
+        matches.get_one::<String>("feature").expect("required")
+    );
 }
