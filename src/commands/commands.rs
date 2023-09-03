b8bc3504b4780783e9ff7e0cb355297536dfb91f
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-09-04 01:23:32 +0545

    remove run method on Commands enum and always make it implicitly run

--- src/commands/commands.rs
+++ src/commands/commands.rs
@@ -4,16 +4,10 @@ pub enum Commands {
     Feature(Box<str>),
 }
 
-impl Commands {
-    pub(crate) fn run(&self) {
+impl Drop for Commands {
+    fn drop(&mut self) {
         match self {
             Self::Feature(feat) => build_feature(feat),
         }
-    }
-}
-
-impl Drop for Commands {
-    fn drop(&mut self) {
-        self.run();
     }
 }
